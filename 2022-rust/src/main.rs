dda7eecb8ff15ab2a64ec0badf9b8b2bf31065ea
Exter-N <exter-n@exter-n.fr>
2023-12-18 02:15:59 +0100

    Restructure

--- 2022-rust/src/main.rs
+++ 2022-rust/src/main.rs
@@ -5,7 +5,31 @@ use std::io;
 use std::{error::Error, io::BufRead};
 
 mod cli;
-mod days;
+mod day01;
+mod day02;
+mod day03;
+mod day04;
+mod day05;
+mod day06;
+mod day07;
+mod day08;
+mod day09;
+mod day10;
+mod day11;
+mod day12;
+mod day13;
+mod day14;
+mod day15;
+mod day16;
+mod day17;
+mod day18;
+mod day19;
+mod day20;
+mod day21;
+mod day22;
+mod day23;
+mod day24;
+mod day25;
 
 use cli::{Cli, Commands};
 
@@ -13,31 +37,31 @@ fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
 
     let day: Day = match cli.command {
-        Commands::Day01 { verbose } => days::day01::new(verbose),
-        Commands::Day02 { gold } => days::day02::new(gold),
-        Commands::Day03 => days::day03::new(),
-        Commands::Day04 => days::day04::new(),
-        Commands::Day05 { gold, verbose } => days::day05::new(gold, verbose),
-        Commands::Day06 { gold } => days::day06::new(gold),
-        Commands::Day07 { verbose } => days::day07::new(verbose),
-        Commands::Day08 { verbosity } => days::day08::new(verbosity),
-        Commands::Day09 { gold, verbosity } => days::day09::new(gold, verbosity),
-        Commands::Day10 { verbose } => days::day10::new(verbose),
-        Commands::Day11 { gold, verbosity } => days::day11::new(gold, verbosity),
-        Commands::Day12 { gold, verbose } => days::day12::new(gold, verbose),
-        Commands::Day13 => days::day13::new(),
-        Commands::Day14 { gold, verbose } => days::day14::new(gold, verbose),
-        Commands::Day15 { sample, verbose } => days::day15::new(sample, verbose),
-        Commands::Day16 => days::day16::new(),
-        Commands::Day17 { gold, verbose } => days::day17::new(gold, verbose),
-        Commands::Day18 => days::day18::new(),
-        Commands::Day19 => days::day19::new(),
-        Commands::Day20 { gold } => days::day20::new(gold),
-        Commands::Day21 { gold } => days::day21::new(gold),
-        Commands::Day22 { gold } => days::day22::new(gold),
-        Commands::Day23 => days::day23::new(),
-        Commands::Day24 => days::day24::new(),
-        Commands::Day25 => days::day25::new(),
+        Commands::Day01 { verbose } => day01::new(verbose),
+        Commands::Day02 { gold } => day02::new(gold),
+        Commands::Day03 => day03::new(),
+        Commands::Day04 => day04::new(),
+        Commands::Day05 { gold, verbose } => day05::new(gold, verbose),
+        Commands::Day06 { gold } => day06::new(gold),
+        Commands::Day07 { verbose } => day07::new(verbose),
+        Commands::Day08 { verbosity } => day08::new(verbosity),
+        Commands::Day09 { gold, verbosity } => day09::new(gold, verbosity),
+        Commands::Day10 { verbose } => day10::new(verbose),
+        Commands::Day11 { gold, verbosity } => day11::new(gold, verbosity),
+        Commands::Day12 { gold, verbose } => day12::new(gold, verbose),
+        Commands::Day13 => day13::new(),
+        Commands::Day14 { gold, verbose } => day14::new(gold, verbose),
+        Commands::Day15 { sample, verbose } => day15::new(sample, verbose),
+        Commands::Day16 => day16::new(),
+        Commands::Day17 { gold, verbose } => day17::new(gold, verbose),
+        Commands::Day18 => day18::new(),
+        Commands::Day19 => day19::new(),
+        Commands::Day20 { gold } => day20::new(gold),
+        Commands::Day21 { gold } => day21::new(gold),
+        Commands::Day22 { gold } => day22::new(gold),
+        Commands::Day23 => day23::new(),
+        Commands::Day24 => day24::new(),
+        Commands::Day25 => day25::new(),
     }?;
 
     println!("--- Day {}: {} ---", day.number, day.title);
--- 2023-rust/src/main.rs
+++ 2023-rust/src/main.rs
@@ -4,7 +4,23 @@ use std::io;
 use std::{error::Error, io::BufRead};
 
 mod cli;
-mod days;
+mod day01;
+mod day02;
+mod day03;
+mod day04;
+mod day05;
+mod day06;
+mod day07;
+mod day08;
+mod day09;
+mod day10;
+mod day11;
+mod day12;
+mod day13;
+mod day14;
+mod day15;
+mod day16;
+mod day17;
 
 use aoc_common_rs::day::Day;
 use cli::{Cli, Commands};
@@ -13,23 +29,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
 
     let day: Day = match cli.command {
-        Commands::Day01 { gold, verbose } => days::day01::new(gold, verbose),
-        Commands::Day02 => days::day02::new(),
-        Commands::Day03 => days::day03::new(),
-        Commands::Day04 { gold } => days::day04::new(gold),
-        Commands::Day05 { gold } => days::day05::new(gold),
-        Commands::Day06 { gold } => days::day06::new(gold),
-        Commands::Day07 { gold } => days::day07::new(gold),
-        Commands::Day08 { gold } => days::day08::new(gold),
-        Commands::Day09 { gold } => days::day09::new(gold),
-        Commands::Day10 => days::day10::new(),
-        Commands::Day11 => days::day11::new(),
-        Commands::Day12 { gold } => days::day12::new(gold),
-        Commands::Day13 { gold } => days::day13::new(gold),
-        Commands::Day14 { gold } => days::day14::new(gold),
-        Commands::Day15 => days::day15::new(),
-        Commands::Day16 { gold } => days::day16::new(gold),
-        Commands::Day17 { gold } => days::day17::new(gold),
+        Commands::Day01 { gold, verbose } => day01::new(gold, verbose),
+        Commands::Day02 => day02::new(),
+        Commands::Day03 => day03::new(),
+        Commands::Day04 { gold } => day04::new(gold),
+        Commands::Day05 { gold } => day05::new(gold),
+        Commands::Day06 { gold } => day06::new(gold),
+        Commands::Day07 { gold } => day07::new(gold),
+        Commands::Day08 { gold } => day08::new(gold),
+        Commands::Day09 { gold } => day09::new(gold),
+        Commands::Day10 => day10::new(),
+        Commands::Day11 => day11::new(),
+        Commands::Day12 { gold } => day12::new(gold),
+        Commands::Day13 { gold } => day13::new(gold),
+        Commands::Day14 { gold } => day14::new(gold),
+        Commands::Day15 => day15::new(),
+        Commands::Day16 { gold } => day16::new(gold),
+        Commands::Day17 { gold } => day17::new(gold),
     }?;
 
     println!("--- Day {}: {} ---", day.number, day.title);
